9a8e43f18a455a9640868a56c44aff3c649d6167
Bendodroid <bendodroid@gmail.com>
2017-07-29 16:18:18 +0200

    - Added SyncDesktop.sh script to VCS

---
+++ src/SyncDesktop.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+rm -r /home/bennett/Schreibtisch/RP
+
+rm -r /home/bennett/IdeaProjects/RP/src/__pycache__/
+
+mkdir /home/bennett/Schreibtisch/RP
+
+cp -r /home/bennett/IdeaProjects/RP/src/ /home/bennett/Schreibtisch/RP
