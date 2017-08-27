a73df3a3f955ab832c717ba54e05f66675454978
Bendodroid <bendodroid@gmail.com>
2017-08-27 16:48:13 +0200

    Status 27/08/2017 16:48

--- src/SyncDesktop.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-rm -r /home/bennett/Schreibtisch/RP
-
-rm -r /home/bennett/IdeaProjects/RP/src/__pycache__/
-
-mkdir /home/bennett/Schreibtisch/RP
-
-cp -r /home/bennett/IdeaProjects/RP/src/ /home/bennett/Schreibtisch/RP
