c674bf85553cbf4312c08a200ad274774ca70649
Bendodroid <bendodroid@gmail.com>
2017-09-26 22:20:20 +0200

    - Updated 2 files

--- src/Units/GameMaster.py
+++ src/Units/GameMaster.py
@@ -2,12 +2,9 @@
 
 # Copyright Bendodroid [2017]
 
-<<<<<<< HEAD
 import Units.Unit
 import Engine.LocationHandler
 
-=======
->>>>>>> 76a9b998a9591929fa9ccad6e35fc8e8ba013edb
 
 class GameMaster(Units.Unit.Unit):
     pass
